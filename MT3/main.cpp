72e7fe6d50fa5f1c50d10a3d44679649b8dd0291
朝聖 屋良 <k023g0060@m.neec.ac.jp>
2024-04-18 20:40:50 +0900

    4/18

--- MT3/main.cpp
+++ MT3/main.cpp
@@ -1,7 +1,19 @@
 #include <Novice.h>
-
+#include<Matrix4x4.h>
+#include<Vector3.h>
 const char kWindowTitle[] = "LE2B_27_ヤラ_チョウセイ";
+// １．平行移動行列
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
 
+}
+// ２．拡大縮小行列
+Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+
+}
+// ３．座標変換
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) {
+
+}
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -24,6 +36,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
+		Vector3 translate{ 4.1f,2.6f,0.8f };
+		Vector3 scale{ 1.5f,5.2f,7.3f };
+		Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+		Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+		Vector3 point{ 2.3f,3.8f,1.4f };
+		Matrix4x4 transformMatrix = {
+			1.0f,2.0f,3.0f,4.0f,
+			3.0f,1.0f,1.0f,2.0f,
+			1.0f,4.0f,2.0f,3.0f,
+			2.0f,2.0f,1.0f,3.0f
+		};
+		Vector3 transformed = Transform(point, transformMatrix);
 
 		///
 		/// ↑更新処理ここまで
