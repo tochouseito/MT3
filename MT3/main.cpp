ff63b7bbe3dff5c6305777059da6904140644963
朝聖 屋良 <k023g0060@m.neec.ac.jp>
2024-04-20 17:12:47 +0900

    4/20

--- MT3/main.cpp
+++ MT3/main.cpp
@@ -136,7 +136,18 @@ Matrix4x4 MakeRotateZMatrix(float radian) {
 	result.m[3][3] = 1.0f;
 	return result;
 }
-
+// 3次元アフィン変換行列
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate,  const Vector3& translate) {
+	Matrix4x4 result;
+	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+	result = Multiply(translateMatrix, Multiply(scaleMatrix, rotateXYZMatrix));
+	return result;
+}
 static const int kRowHeight = 20;
 static const int kColumnWidth = 60;
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix) {
@@ -160,7 +171,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
-
+	Vector3 scale{ 1.2f,0.79f,-2.1f };
+	Vector3 rotate{ 0.4f,1.43f,-0.8f };
+	Vector3 translate{ 2.7f,-4.15f,1.57f };
+	Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -173,11 +187,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-		Vector3 rotate{ 0.4f,1.43f,-0.0f };
-		Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
-		Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
-		Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
-		Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+		
 
 		///
 		/// ↑更新処理ここまで
@@ -186,10 +196,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		MatrixScreenPrintf(0, 0, rotateXMatrix);
-		MatrixScreenPrintf(0, kRowHeight * 5, rotateYMatrix);
-		MatrixScreenPrintf(0, kRowHeight * 5 * 2, rotateZMatrix);
-		MatrixScreenPrintf(0, kRowHeight * 5 * 3, rotateXYZMatrix);
+		MatrixScreenPrintf(0, 0, worldMatrix);
+		
 		///
 		/// ↑描画処理ここまで
 		///
