6d072f81a27d381a8eebeb87fcfe0a963c1672fc
tochouseito <k023g0060@g.neec.ac.jp>
2024-04-26 10:40:18 +0900

    1

--- MT3/main.cpp
+++ MT3/main.cpp
@@ -214,6 +214,119 @@ Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, f
 	result.m[3][3] = 1.0f;
 	return result;
 }
+// 逆行列
+Matrix4x4 Inverse(const Matrix4x4& m) {
+	Matrix4x4 result = { 0 };
+	/* 逆行列を求める行列用の２次元配列 */
+	double mat[N][N];
+
+	/* 逆行列用の２次元配列 */
+	double inv[N][N];
+
+	/* 掃き出し法を行う行列 */
+	double sweep[N][N * 2];
+
+	int i; /* 行 */
+	int j; /* 列 */
+	int k; /* 注目対角成分が存在する列 */
+
+	double a; /* 定数倍用 */
+
+	/* 正方行列の各成分をセット */
+	mat[0][0] = m.m[0][0]; mat[0][1] = m.m[0][1]; mat[0][2] = m.m[0][2]; mat[0][3] = m.m[0][3];
+	mat[1][0] = m.m[1][0]; mat[1][1] = m.m[1][1]; mat[1][2] = m.m[1][2]; mat[1][3] = m.m[1][3];
+	mat[2][0] = m.m[2][0]; mat[2][1] = m.m[2][1]; mat[2][2] = m.m[2][2]; mat[2][3] = m.m[2][3];
+	mat[3][0] = m.m[3][0]; mat[3][1] = m.m[3][1]; mat[3][2] = m.m[3][2]; mat[3][3] = m.m[3][3];
+
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			/* sweepの左側に逆行列を求める行列をセット */
+			sweep[i][j] = mat[i][j];
+
+			/* sweepの右側に単位行列をセット */
+			sweep[i][N + j] = (i == j) ? 1 : 0;
+		}
+	}
+
+
+	/* 全ての列の対角成分に対する繰り返し */
+	for (k = 0; k < N; k++) {
+
+		/* 最大の絶対値を注目対角成分の絶対値と仮定 */
+		double max = fabs(sweep[k][k]);
+		int max_i = k;
+
+		/* k列目が最大の絶対値となる行を探す */
+		for (i = k + 1; i < N; i++) {
+			if (fabs(sweep[i][k]) > max) {
+				max = fabs(sweep[i][k]);
+				max_i = i;
+			}
+		}
+
+
+
+		/* 操作（１）：k行目とmax_i行目を入れ替える */
+		if (k != max_i) {
+			for (j = 0; j < N * 2; j++) {
+				double tmp = sweep[max_i][j];
+				sweep[max_i][j] = sweep[k][j];
+				sweep[k][j] = tmp;
+			}
+		}
+
+		/* sweep[k][k]に掛けると1になる値を求める */
+		a = 1 / sweep[k][k];
+
+		/* 操作（２）：k行目をa倍する */
+		for (j = 0; j < N * 2; j++) {
+			/* これによりsweep[k][k]が1になる */
+			sweep[k][j] *= a;
+		}
+
+		/* 操作（３）によりk行目以外の行のk列目を0にする */
+		for (i = 0; i < N; i++) {
+			if (i == k) {
+				/* k行目はそのまま */
+				continue;
+			}
+
+			/* k行目に掛ける値を求める */
+			a = -sweep[i][k];
+
+			for (j = 0; j < N * 2; j++) {
+				/* i行目にk行目をa倍した行を足す */
+				/* これによりsweep[i][k]が0になる */
+				sweep[i][j] += sweep[k][j] * a;
+			}
+		}
+	}
+
+	/* sweepの右半分がmatの逆行列 */
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			inv[i][j] = sweep[i][N + j];
+		}
+	}
+
+	/* 逆行列invを表示 */
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			result.m[i][j] = float(inv[i][j]);
+			//printf("%0.2f, ", inv[i][j]);
+		}
+		//printf("\n");
+	}
+
+	/* 検算 */
+	if (check(mat, inv)) {
+		//printf("invはmatの逆行列です！！\n");
+	}
+	else {
+		//printf("invはmatの逆行列になってません...\n");
+	}
+	return result;
+}
 static const int kRowHeight = 20;
 static const int kColumnWidth = 60;
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix) {
