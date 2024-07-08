2853384f14959a79dc74ee38cedd23124c5c2cbf
tochouseito <k023g0060@g.neec.ac.jp>
2024-07-08 16:17:09 +0900

    修正

--- MT3/Matrix.h
+++ MT3/Matrix.h
@@ -598,7 +598,6 @@ bool IsCollision(const Sphere& s1, const Sphere& s2) {
 bool IsCollision(const Sphere& sphere, const Plane& plane) {
 	// 2つの円と平面の中心間の距離を計算
 	float distance = std::abs((Dot(plane.normal, sphere.center)) - plane.distance);
-	//float distance = float(std::sqrt(std::pow(plane.normal.x - sphere.center.x, 2) + std::pow(plane.normal.y - sphere.center.y, 2) + std::pow(plane.normal.z - sphere.center.z, 2)));
 	// 中心間の距離が2つの円の半径の合計よりも小さい場合、衝突しているとみなす
 	if (distance <= sphere.radius) {
 		return true;
