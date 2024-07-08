4bb61b60721011461b87f952a2b1e73190c8ce38
tochouseito <k023g0060@g.neec.ac.jp>
2024-07-08 13:01:11 +0900

    応用完成

--- MT3/main.cpp
+++ MT3/main.cpp
@@ -21,13 +21,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Vector2Int mouse;
 
 	Spring spring{};
-	spring.anchor = { 0.0f,0.0f,0.0f };
-	spring.naturalLength = 1.0f;
+	spring.anchor = { 0.0f,1.0f,0.0f };
+	spring.naturalLength = 0.7f;
 	spring.stiffness = 100.0f;
 	spring.dampingCoefficient = 2.0f;
 
 	Ball ball{};
-	ball.position = { 1.2f,0.0f,0.0f };
+	ball.position = { 0.8f,0.2f,0.0f };
 	ball.mass = 2.0f;
 	ball.radius = 0.05f;
 	ball.color = BLUE;
@@ -35,7 +35,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	bool move = false;
 	Sphere sphere{};
 	float deltaTime = 1.0f / 60.0f;
-
+	const Vector3 kGravity = { 0.0f,-9.8f,0.0f };
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
@@ -71,7 +71,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 				Vector3 displacement = length * (ball.position - restPosition);
 				Vector3 restoringForce = -spring.stiffness * displacement;
 				Vector3 dampingForce = -spring.dampingCoefficient * ball.velocity;
-				Vector3 force = restoringForce + dampingForce;
+				Vector3 gravityForce = kGravity * ball.mass;
+				Vector3 force = restoringForce + dampingForce+gravityForce;
 				ball.acceleration = force / ball.mass;
 
 			}
@@ -81,6 +82,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		}
 		sphere.center = ball.position;
 		sphere.radius = ball.radius;
+		Vector3 transformedPrev = Transform(sphere.center, ViewProjectionMatrix);
+		transformedPrev = Transform(transformedPrev, viewportMatrix);
+		Vector3 transformedPrev1 = Transform(spring.anchor, ViewProjectionMatrix);
+		transformedPrev1 = Transform(transformedPrev1, viewportMatrix);
 		///
 		/// ↑更新処理ここまで
 		///
@@ -102,7 +107,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		}
 
 		ImGui::End();
-		
+		Novice::DrawLine(static_cast<int>(transformedPrev.x), static_cast<int>(transformedPrev.y),
+			static_cast<int>(transformedPrev1.x), static_cast<int>(transformedPrev1.y), WHITE);
 		DrawSphere(sphere, ViewProjectionMatrix, viewportMatrix, ball.color);
 		DrawGrid(ViewProjectionMatrix, viewportMatrix);
 		///
