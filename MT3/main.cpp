0536339d6b746bf1f606d0a7a8da87c59d95ebd3
tochouseito <k023g0060@g.neec.ac.jp>
2024-07-08 12:09:20 +0900

    完了

--- MT3/main.cpp
+++ MT3/main.cpp
@@ -17,32 +17,20 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Vector3 cameraTranslate{ 0.0f, 1.9f, -6.49f };
 	Vector3 cameraRotate{ 0.26f, 0.0f, 0.0f };
 	Vector3 cameraPosition{ 0.0f, 1.0f, -5.0f };
-	
-	Vector3 translates[3] = {
-		{0.2f,1.0f,0.0f},
-		{0.4f,0.0f,0.0f},
-		{0.3f,0.0f,0.0f}
-	};
-	Vector3 rotates[3] = {
-		{0.0f,0.0f,-6.8f},
-		{0.0f,0.0f,-1.4f},
-		{0.0f,0.0f,0.0f},
-	};
-	Vector3 scales[3] = {
-		{1.0f,1.0f,1.0f},
-		{1.0f,1.0f,1.0f},
-		{1.0f,1.0f,1.0f},
-	};
-	Sphere sphere[3];
-	sphere[0].center = translates[0];
-	sphere[1].center = translates[1];
-	sphere[2].center = translates[2];
-	sphere[0].radius = 0.1f;
-	sphere[1].radius = 0.1f;
-	sphere[2].radius = 0.1f;
 	static bool isDebugCamera = false;
-	Vector3 rotate{ 0.0f,0.0f,0.0f };
 	Vector2Int mouse;
+
+	Vector3 a{ 0.2f,1.0f,0.0f };
+	Vector3 b{ 2.4f,3.1f,1.2f };
+	Vector3 c = a + b;
+	Vector3 d = a - b;
+	Vector3 e = a * 2.4f;
+	Vector3 rotate{ 0.4f,1.43f,-0.8f };
+	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+	Matrix4x4 rotateMatrix = rotateXMatrix * rotateYMatrix * rotateZMatrix;
+
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
@@ -69,30 +57,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
 		Matrix4x4 ViewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
 		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
-		// 回転行列を生成
-		Matrix4x4 rotateMatrix = Multiply(MakeRotateXMatrix(rotate.x), Multiply(MakeRotateYMatrix(rotate.y), MakeRotateZMatrix(rotate.z)));
-
-		Matrix4x4 SRT0 = MakeAffineMatrix(scales[0], rotates[0], translates[0]);
-		Matrix4x4 SRT1 = MakeAffineMatrix(scales[1], rotates[1], translates[1]);
-		Matrix4x4 SRT2 = MakeAffineMatrix(scales[2], rotates[2], translates[2]);
-		Matrix4x4 L0 = SRT0;
-		Matrix4x4 L1 = Multiply(SRT1, L0);
-		Matrix4x4 L2 = Multiply(SRT2, L1);
-
-		Vector3 translations[3];
-		translations[0] = { L0.m[3][0],L0.m[3][1],L0.m[3][2] };
-		translations[1] = { L1.m[3][0],L1.m[3][1],L1.m[3][2] };
-		translations[2] = { L2.m[3][0],L2.m[3][1],L2.m[3][2] };
-
-		Vector3 transformedPrev0 = Transform(translations[0], ViewProjectionMatrix);
-		transformedPrev0 = Transform(transformedPrev0, viewportMatrix);
-		Vector3 transformedPrev1 = Transform(translations[1], ViewProjectionMatrix);
-		transformedPrev1 = Transform(transformedPrev1, viewportMatrix);
-		Vector3 transformedPrev2 = Transform(translations[2], ViewProjectionMatrix);
-		transformedPrev2 = Transform(transformedPrev2, viewportMatrix);
-		sphere[0].center = translations[0];
-		sphere[1].center = translations[1];
-		sphere[2].center = translations[2];
+		
 		
 		///
 		/// ↑更新処理ここまで
@@ -102,28 +67,25 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 		
-		ImGui::Begin("Window");
+		ImGui::Begin("Camera");
 		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
 		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
 		ImGui::DragFloat3("CameraPosition", &cameraPosition.x, 0.01f);
-		
-		ImGui::DragFloat3("translates0", &translates[0].x, 0.01f);
-		ImGui::DragFloat3("rotate0", &rotates[0].x, 0.01f);
-		ImGui::DragFloat3("translates1", &translates[1].x, 0.01f);
-		ImGui::DragFloat3("rotate1", &rotates[1].x, 0.01f);
-		ImGui::DragFloat3("translates2", &translates[2].x, 0.01f);
-		ImGui::DragFloat3("rotate2", &rotates[2].x, 0.01f);
+		ImGui::End();
 
+		ImGui::Begin("Window");
+		ImGui::Text("c:%f,%f,%f", c.x, c.y, c.z);
+		ImGui::Text("d:%f,%f,%f", d.x, d.y, d.z);
+		ImGui::Text("e:%f,%f,%f", e.x, e.y, e.z);
+		ImGui::Text("Matrix:\n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f\n",
+			rotateMatrix.m[0][0], rotateMatrix.m[0][1], rotateMatrix.m[0][2],
+			rotateMatrix.m[0][3], rotateMatrix.m[1][0], rotateMatrix.m[1][1],
+			rotateMatrix.m[1][2], rotateMatrix.m[1][3], rotateMatrix.m[2][0],
+			rotateMatrix.m[2][1], rotateMatrix.m[2][2], rotateMatrix.m[2][3],
+			rotateMatrix.m[3][0], rotateMatrix.m[3][1], rotateMatrix.m[3][2],
+			rotateMatrix.m[3][3]);
 		ImGui::End();
 		
-		DrawSphere(sphere[0], ViewProjectionMatrix, viewportMatrix, RED);
-		DrawSphere(sphere[1], ViewProjectionMatrix, viewportMatrix, GREEN);
-		DrawSphere(sphere[2], ViewProjectionMatrix, viewportMatrix, BLUE);
-
-		Novice::DrawLine(static_cast<int>(transformedPrev0.x), static_cast<int>(transformedPrev0.y),
-			static_cast<int>(transformedPrev1.x), static_cast<int>(transformedPrev1.y), WHITE);
-		Novice::DrawLine(static_cast<int>(transformedPrev1.x), static_cast<int>(transformedPrev1.y),
-			static_cast<int>(transformedPrev2.x), static_cast<int>(transformedPrev2.y), WHITE);
 		DrawGrid(ViewProjectionMatrix, viewportMatrix);
 		///
 		/// ↑描画処理ここまで
